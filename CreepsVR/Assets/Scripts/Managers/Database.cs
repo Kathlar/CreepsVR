ac95a6c2d038dc689473404d22ffbdc67270399e
Kathlar <kathlar@superhotgame.com>
2020-11-07 23:28:20 +0100

    [+] Game, Database scripts created

---
+++ CreepsVR/Assets/Scripts/Managers/Database.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "GameDatabase", menuName = "Kathlar/_GameDatabase")]
+public class Database : ScriptableObject
+{
+    public static Database database { get { return Game.GameDatabase; } }
+    public List<PlayerInformation> playerInfos = new List<PlayerInformation>();
+    public static List<PlayerInformation> PlayerInfos { get { return database.playerInfos; } }
+    [System.Serializable]
+    public class LevelInformations
+    {
+        public LevelInformation mainMenuLevelInfo;
+        public List<LevelInformation> gameLevelInfos = new List<LevelInformation>();
+    }
+    public LevelInformations levelInformations;
+    public static LevelInformations Levels { get { return database.levelInformations; } }
+    
+    public List<GameObject> weaponPrefabs = new List<GameObject>();
+    public static List<GameObject> WeaponPrefabs { get { return database.weaponPrefabs; } }
+}
+
+[System.Serializable]
+public class PlayerInformation
+{
+    public Color color = Color.white;
+    public Material material;
+}
+
+[System.Serializable]
+public class LevelInformation
+{
+    public string sceneAssetName;
+    public string levelName;
+}
---
+++ CreepsVR/Assets/Scripts/Managers/Game.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : Singleton<Game>
+{
+    public Database database;
+    public static Database GameDatabase { get { return Instance.database; } }
+
+    protected override void SingletonAwake()
+    {
+
+    }
+}

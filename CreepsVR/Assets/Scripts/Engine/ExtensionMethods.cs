b0db00345e7ac367e65fb2f25f8e0e3b549db8cb
Kathlar <kathlar@superhotgame.com>
2020-11-07 22:54:55 +0100

    [+] ExtensionMethods script created

---
+++ CreepsVR/Assets/Scripts/Engine/ExtensionMethods.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public static class ExtensionMethods
+{
+    public static void ResetLocalTransform(this Transform trans)
+    {
+        trans.localPosition = Vector3.zero;
+        trans.localRotation = Quaternion.identity;
+        trans.localScale = Vector3.one;
+    }
+
+    public static Vector3 FlatY(this Vector3 vector3)
+    {
+        vector3.y = 0;
+        return vector3;
+    }
+}

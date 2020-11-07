f45a84aeaf9fecec95f72c53466f0f781ce5a141
Kathlar <kathlar@superhotgame.com>
2020-11-07 22:54:45 +0100

    [+] Singleton class created

---
+++ CreepsVR/Assets/Scripts/Engine/Singleton.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public abstract class Singleton<T> : SingletonBase where T : SingletonBase
+{
+    private static T instance;
+    protected static T Instance { get { return instance; } }
+
+    protected sealed override void Awake()
+    {
+        instance = this as T;
+        base.Awake();
+    }
+}
+
+public abstract class SingletonBase : MonoBehaviour
+{
+    protected virtual void Awake()
+    {
+        SingletonAwake();
+    }
+
+    protected abstract void SingletonAwake();
+}

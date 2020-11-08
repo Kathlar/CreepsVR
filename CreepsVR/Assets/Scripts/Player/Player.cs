ebbf3b4a701779e10c9014d24f5720a6d8a77338
Kathlar <tomkathlar@gmail.com>
2020-11-08 01:32:55 +0100

    [+] Player script added

---
+++ CreepsVR/Assets/Scripts/Player/Player.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Player : MonoBehaviour
+{
+    public Camera mainCamera { get; private set; }
+
+    protected virtual void Awake()
+    {
+        mainCamera = GetComponentInChildren<Camera>();
+    }
+}

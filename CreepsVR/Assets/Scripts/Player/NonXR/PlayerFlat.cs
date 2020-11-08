0a1617d7ed3f20969fdb2840149825c7f260f3ce
Kathlar <tomkathlar@gmail.com>
2020-11-08 02:18:38 +0100

    [+] Flat Player rotation added

--- CreepsVR/Assets/Scripts/Player/NonXR/PlayerFlat.cs
+++ CreepsVR/Assets/Scripts/Player/NonXR/PlayerFlat.cs
@@ -6,10 +6,17 @@ public class PlayerFlat : Player
 {
     public override PlayerType playerType { get { return PlayerType.flat; } }
 
+    private float rotationX, rotationY;
+
     protected override void Update()
     {
         rotationValue = Inputs.SecondaryHorizontal;
 
         base.Update();
+
+        rotationX += Inputs.SecondaryVertical * rotationSpeed;
+        rotationY += Inputs.SecondaryHorizontal * rotationSpeed;
+        rotationX = Mathf.Clamp(rotationX, -90, 90);
+        mainCamera.transform.localEulerAngles = new Vector3(-rotationX, rotationY, 0);
     }
 }

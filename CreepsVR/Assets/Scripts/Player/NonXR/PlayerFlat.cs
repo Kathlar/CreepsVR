8cba4b6bd90766b5cb63bff7f4b16285f78da3da
Kathlar <tomkathlar@gmail.com>
2020-11-08 01:50:43 +0100

    [+] PlayerFlat script created for nonvr testing

---
+++ CreepsVR/Assets/Scripts/Player/NonXR/PlayerFlat.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFlat : Player
+{
+    protected override void Update()
+    {
+        rotationValue = Inputs.SecondaryHorizontal;
+
+        base.Update();
+    }
+}

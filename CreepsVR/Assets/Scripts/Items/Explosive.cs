add0f2df351713b8007e66735729e7548d6593c0
Kathlar <kathlar@superhotgame.com>
2020-11-11 23:22:36 +0100

    [*] Prototype map remade

---
+++ CreepsVR/Assets/Scripts/Items/Explosive.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosive : MonoBehaviour
+{
+    public bool exploded { get; private set; }
+
+    public float timeToExplode = 3;
+    public float explosionRadius = 4;
+    public int damagePower = 20;
+
+    public ParticleSystem explosionEffect;
+
+    public void Explode()
+    {
+        StartCoroutine(ThrownCoroutine());
+    }
+
+    private IEnumerator ThrownCoroutine()
+    {
+        yield return new WaitForSeconds(timeToExplode);
+
+        explosionEffect.gameObject.SetActive(true);
+        explosionEffect.transform.SetParent(null);
+        explosionEffect.Play();
+        Destroy(explosionEffect.gameObject, explosionEffect.duration);
+
+        Collider[] objectsInRadius = Physics.OverlapSphere(transform.position, explosionRadius);
+
+        foreach (Collider objectInRadius in objectsInRadius)
+        {
+            if (objectInRadius.TryGetComponent(out IDamageable damageable))
+                damageable.GetDamage(damagePower, transform.position, Vector3.zero);
+            else if (objectInRadius.TryGetComponent(out Rigidbody hitRB))
+                hitRB.AddExplosionForce(50, transform.position, explosionRadius);
+        }
+
+        exploded = true;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, explosionRadius);
+    }
+}
--- CreepsVR/Assets/Scripts/Items/Grenade.cs
+++ CreepsVR/Assets/Scripts/Items/Grenade.cs
@@ -2,51 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Explosive))]
 public class Grenade : Throwable
 {
-    bool exploded = false;
-    public float timeToExplode = 3;
-    public float explosionRadius = 4;
-    public int damagePower = 20;
+    public Explosive explosive { get; private set; }
 
-    public ParticleSystem explosionEffect;
+    protected override void Awake()
+    {
+        base.Awake();
+        explosive = GetComponent<Explosive>();
+    }
 
     public override void UseEnd()
     {
         base.UseEnd();
-        StartCoroutine(ThrownCoroutine());
-    }
-
-    private IEnumerator ThrownCoroutine()
-    {
-        yield return new WaitForSeconds(timeToExplode);
-
-        explosionEffect.gameObject.SetActive(true);
-        explosionEffect.transform.SetParent(null);
-        explosionEffect.Play();
-        Destroy(explosionEffect.gameObject, explosionEffect.duration);
-
-        Collider[] objectsInRadius = Physics.OverlapSphere(transform.position, explosionRadius);
-
-        foreach(Collider objectInRadius in objectsInRadius)
-        {
-            if (objectInRadius.TryGetComponent(out IDamageable damageable))
-                damageable.GetDamage(damagePower, transform.position, Vector3.zero);
-            else if (objectInRadius.TryGetComponent(out Rigidbody hitRB))
-                hitRB.AddExplosionForce(50, transform.position, explosionRadius);
-        }
-
-        exploded = true;
+        explosive.Explode();
     }
 
     public override bool StillUsing()
     {
-        return !exploded;
-    }
-
-    private void OnDrawGizmos()
-    {
-        Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(transform.position, explosionRadius);
+        return !explosive.exploded;
     }
 }
